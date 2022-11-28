72cc2f0fe6e812f475a75879d9c0bf55ad417754
Ilya Mikhasik <imikhassik@gmail.com>
2022-11-28 07:27:36 -0500

    Update Board class

--- battleship.py
+++ battleship.py
@@ -1,15 +1,13 @@
 class Board:
-    _board_1 = [['O'] * 6 for i in range(6)]
-    _board_2 = [['O'] * 6 for j in range(6)]
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self):
+        self.board = [['O'] * 6 for i in range(6)]
 
     def get_board(self):
-        return self._board_1 if self.name == 'user' else self._board_2
+        return self.board
 
 
-def print_board(board_1, board_2):
+def print_boards(board_1, board_2):
     print('  | 1 | 2 | 3 | 4 | 5 | 6 |       | 1 | 2 | 3 | 4 | 5 | 6 |\n')
     for i in range(6):
         print(i + 1, end=' | ')
@@ -21,9 +19,6 @@ def print_board(board_1, board_2):
         print('\n')
 
 
-user_board = Board('user')
-ai_board = Board('ai')
-user_board._board_1[0][0] = 'x'
-ai_board._board_2[1][1] = 'y'
-
-print_board(user_board, ai_board)
+user_board = Board()
+ai_board = Board()
+print_boards(user_board, ai_board)
