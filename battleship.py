d54d2c3f0c6434c9ece06ec2221382bb92adb382
Ilya Mikhasik <imikhassik@gmail.com>
2022-11-30 10:09:52 -0500

    Implement ai_board setup_fleet logic

--- battleship.py
+++ battleship.py
@@ -1,3 +1,6 @@
+from random import randint, choice
+
+
 def instructions():
     print("-------------------------------------------------")
     print("                  МОРСКОЙ БОЙ                    ")
@@ -19,10 +22,10 @@ class Board:
     def get_board(self):
         return self.board
 
-    def place_ship(self, ship):
+    def place_ship(self, ship, board):
         try:
             while ship.size:
-                if ship.radar():
+                if ship.radar(board):
                     self.board[ship.x][ship.y] = '*'
                     if ship.direction == 'v' and ship.size > 1:
                         ship.x += 1
@@ -75,31 +78,37 @@ class Ship:
         self.direction = direction
         self.size = size
 
-    def get_coordinates(self):
-        while True:
-            try:
-                if self.size > 1:
-                    self.x, self.y, self.direction = input("Введите координаты и направление корабля: ").split()
-                else:
-                    self.x, self.y = input("Введите координаты корабля: ").split()
-                self.x, self.y = map(int, (self.x, self.y))
-                if self.x in range(1, 7) and self.y in range(1, 7) and self.direction in ('h', 'v'):
-                    self.x -= 1
-                    self.y -= 1
-                    break
-                elif self.direction not in ('h', 'v'):
-                    print("Направление: h - по горизонтали, v - по вертикали")
-                else:
-                    print("Координаты за пределами поля")
-            except ValueError:
-                if self.size > 1:
-                    print("Некорректные координаты. Пример корректного ввода: 2 1 h")
-                else:
-                    print("Некорректные координаты. Пример корректного ввода для корабля на одну клетку: 2 1")
-                continue
+    def get_coordinates(self, board):
+        if board == user_board:
+            while True:
+                try:
+                    if self.size > 1:
+                        self.x, self.y, self.direction = input("Введите координаты и направление корабля: ").split()
+                    else:
+                        self.x, self.y = input("Введите координаты корабля: ").split()
+                    self.x, self.y = map(int, (self.x, self.y))
+                    if self.x in range(1, 7) and self.y in range(1, 7) and self.direction in ('h', 'v'):
+                        self.x -= 1
+                        self.y -= 1
+                        break
+                    elif self.direction not in ('h', 'v'):
+                        print("Направление: h - по горизонтали, v - по вертикали")
+                    else:
+                        print("Координаты за пределами поля")
+                except ValueError:
+                    if self.size > 1:
+                        print("Некорректные координаты. Пример корректного ввода: 2 1 h")
+                    else:
+                        print("Некорректные координаты. Пример корректного ввода для корабля на одну клетку: 2 1")
+                    continue
+        elif board == ai_board:
+            if self.size > 1:
+                self.x, self.y, self.direction = randint(0, 5), randint(0, 5), choice(['h', 'v'])
+            else:
+                self.x, self.y = randint(0, 5), randint(0, 5)
 
-    def radar(self):
-        search_area = user_board.get_board().copy()
+    def radar(self, board):
+        search_area = board.get_board().copy()
         if 0 < self.x < 5 and 0 < self.y < 5:
             search_area = [row[self.y-1:self.y+2] for row in search_area[self.x-1:self.x+2]]
         elif self.x == 0 and self.y == 0:
@@ -120,41 +129,49 @@ class Ship:
             search_area = [row[self.y-1:self.y+1] for row in search_area[self.x-1:self.x+2]]
 
         for row in search_area:
-            if '■' in row:
+            if '■' in row and board == user_board:
                 print("Другой корабль слишком близко")
                 reload = input("Очистить поле? (y/n): ").lower()
                 if reload == 'y':
-                    user_board.__init__()
-                    Ship.setup_fleet(1)
+                    board.__init__()
+                    Ship.setup_fleet(board, 1)
                 else:
                     return False
+            elif '■' in row and board == ai_board:
+                return False
         else:
             return True
 
     @staticmethod
-    def setup_fleet(reload=0):
+    def setup_fleet(board, reload=0):
         ships = {}
         sizes = [3, 2, 2, 1, 1, 1, 1]
         i = 0
+        attempts = 10
         while i <= 6:
             if reload:
                 i = 0
                 reload = 0
-                user_board.print_boards()
+                board.print_boards()
             ships[i] = Ship()
             ships[i].size = sizes[i]
-            ships[i].get_coordinates()
-            if user_board.place_ship(ships[i]):
-                user_board.confirm_placement(1)
-                user_board.print_boards()
+            ships[i].get_coordinates(board)
+            if board.place_ship(ships[i], board):
+                board.confirm_placement(1)
+                board.print_boards()
                 i += 1
             else:
-                user_board.confirm_placement(0)
-                continue
+                while attempts:
+                    board.confirm_placement(0)
+                    attempts -= 1
+                    if attempts == 1:
+                        reload = 1
+                    continue
 
 
 instructions()
 user_board = Board()
 ai_board = Board()
 user_board.print_boards()
-Ship.setup_fleet()
+Ship.setup_fleet(user_board)
+Ship.setup_fleet(ai_board)
