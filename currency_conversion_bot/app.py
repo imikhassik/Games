0f01b53b48d0ad777a59b8aa44e5cbf7f50a5134
Ilya Mikhasik <imikhassik@gmail.com>
2022-12-26 03:38:10 -0500

    Handle /values

--- currency_conversion_bot/app.py
+++ currency_conversion_bot/app.py
@@ -4,11 +4,23 @@ from config import TOKEN
 
 bot = telebot.TeleBot(TOKEN)
 
+currencies = {'евро': 'EUR',
+              'доллар': 'USD',
+              'рубль': 'RUB'}
+
 
 @bot.message_handler(commands=['start', 'help'])
-def help_func(message: telebot.types.Message):
+def get_help(message: telebot.types.Message):
     text = '''Формат ввода:\n<какую валюту конвертировать> \
 <в какую валюту конвертировать> <количество>'''
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['values'])
+def get_values(message: telebot.types.Message):
+    text = 'Список возможных валют:'
+    for value in currencies.keys():
+        text = ('\n'.join([text, value]))
     bot.reply_to(message, text)
 
 
