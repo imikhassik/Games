d39d241c97e23dd1e875815047877a85eebcf291
Ilya Mikhasik <imikhassik@gmail.com>
2022-12-26 04:10:04 -0500

    Handle user input

--- currency_conversion_bot/app.py
+++ currency_conversion_bot/app.py
@@ -1,12 +1,10 @@
 import telebot
-from config import TOKEN
+
+
+from config import TOKEN, currencies
 
 
 bot = telebot.TeleBot(TOKEN)
-
-currencies = {'евро': 'EUR',
-              'доллар': 'USD',
-              'рубль': 'RUB'}
 
 
 @bot.message_handler(commands=['start', 'help'])
@@ -22,6 +20,12 @@ def get_values(message: telebot.types.Message):
     for value in currencies.keys():
         text = ('\n'.join([text, value]))
     bot.reply_to(message, text)
+
+
+@bot.message_handler(content_types=['text'])
+def get_input(message: telebot.types.Message):
+    user_input = message.text.split(' ')
+    base, quote, amount = user_input[0], user_input[1], user_input[2]
 
 
 bot.polling()
